d2bd80b8e8f12c645e3fbf75a075170cbfc82766
Luis Escobar <lae311@stern.nyu.edu>
2021-11-23 16:50:51 -0500

    commit tree 2e72371464175b60ca09a0aeadd058cd3cd8ffe9

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
+class Book
+    # these attributes are properties. What you initialize with is the reader and those that you dont are set with accessor
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    
+    def initialize(title)
+        @title = title
+    end
+    # methods are hashtags that dont have to be named in the attr
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
